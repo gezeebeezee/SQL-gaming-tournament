c8ccec79045a21369a8ba90eb63e4e7e7aeb96aa
Steven Vu <110693230+slv87645@users.noreply.github.com>
2023-11-03 14:26:39 -0400

    Add files via upload

---
+++ DB_SQL/DDL.sql
@@ -0,0 +1,155 @@
+-- Group 73 Project Step 2
+-- Steven Vu and Kevin Le
+
+--
+--
+
+-- --------------------------------------------------------
+SET FOREIGN_KEY_CHECKS = 0;
+SET AUTOCOMMIT = 0;
+
+--
+-- Table structure for table `Tournaments`
+--
+
+CREATE OR REPLACE TABLE Tournaments (
+  TournamentID int AUTO_INCREMENT NOT NULL UNIQUE,
+  TournamentName varchar(45) NOT NULL,
+  Location varchar(45) NOT NULL,
+  StartDate date NOT NULL,
+  EndDate date NOT NULL,
+  PRIMARY KEY (TournamentID)
+);
+
+--
+-- Table structure for table `Games`
+--
+
+CREATE OR REPLACE TABLE Games (
+  GameID int AUTO_INCREMENT NOT NULL UNIQUE,
+  Title varchar(45) NOT NULL UNIQUE,
+  Genre varchar(45) NOT NULL,
+  Platform varchar(45) NOT NULL,
+  PRIMARY KEY (GameID)
+);
+
+--
+-- Table structure for table `Sponsors`
+--
+
+CREATE OR REPLACE TABLE Sponsors (
+  SponsorID int AUTO_INCREMENT NOT NULL UNIQUE,
+  SponsorName varchar(45) NOT NULL UNIQUE,
+  ContactPerson varchar(45) NOT NULL,
+  ContactEmail varchar(45) NOT NULL,
+  PRIMARY KEY (SponsorID)
+);
+
+--
+-- Table structure for table `Teams`
+--
+
+CREATE OR REPLACE TABLE Teams (
+  TeamID int AUTO_INCREMENT NOT NULL UNIQUE,
+  TeamName varchar(50) NOT NULL UNIQUE,
+  Captain varchar(45) NOT NULL,
+  Location varchar(45) NOT NULL,
+  SponsorID int,
+  TournamentID int,
+  Description varchar(100) DEFAULT NULL,
+  PRIMARY KEY (TeamID),
+   FOREIGN KEY (SponsorID) REFERENCES Sponsors(SponsorID)
+    ON DELETE CASCADE,
+  FOREIGN KEY (TournamentID) REFERENCES Tournaments(TournamentID)
+    ON DELETE CASCADE
+);
+
+--
+-- Table structure for table 'Players'
+--
+
+CREATE OR REPLACE TABLE Players (
+  PlayerID int AUTO_INCREMENT NOT NULL UNIQUE,
+  Name varchar(50) NOT NULL,
+  Username varchar(50) NOT NULL UNIQUE,
+  Birthdate date NOT NULL,
+  TeamID int,
+  GameID int,
+  PRIMARY KEY (PlayerID),
+  FOREIGN KEY (GameID) REFERENCES Games(GameID)
+    ON DELETE CASCADE,
+  FOREIGN KEY (TeamID) REFERENCES Teams(TeamID)
+    ON DELETE CASCADE
+);
+
+--
+-- Table structure for table `Tournaments_has_Games`
+--
+
+CREATE OR REPLACE TABLE Tournaments_has_Games (
+  GameID int NOT NULL,
+  TournamentID int NOT NULL,
+  PRIMARY KEY (GameID, TournamentID),
+  FOREIGN KEY (GameID) REFERENCES Games(GameID)
+    ON DELETE CASCADE,
+  FOREIGN KEY (TournamentID) REFERENCES Tournaments(TournamentID) 
+    ON DELETE CASCADE
+);
+
+
+--
+-- Insert into 'Games'
+INSERT INTO Games (Title, Genre, Platform) VALUES
+('League of Legends', 'MOBA', 'PC'),
+('Valorant', 'Shooter', 'PC'),
+('Super Smash Bros', 'Fighting', 'Nintendo Switch'),
+('Yu-Gi-Oh', 'Card', 'Tabletop');
+
+--
+-- Insert into `Tournaments`
+--
+INSERT INTO Tournaments (TournamentName, Location, StartDate, EndDate) VALUES
+('Momocon', 'Atlanta', '2023-01-01', '2023-01-03'),
+('DreamHack', 'Chicago', '2023-01-01', '2023-01-03'),
+('E3', 'New York', '2023-01-01', '2023-01-03'),
+('DragonCon', 'Atlanta', '2023-09-09', '2023-09-12');
+
+--
+-- Insert into `Tournaments_has_Games`
+--
+
+INSERT INTO Tournaments_has_Games (GameID, TournamentID) VALUES
+(1, 3),
+(2, 2),
+(3, 3),
+(4, 2);
+
+--
+-- Insert into 'Sponsors'
+--
+INSERT INTO Sponsors (SponsorName, ContactPerson, ContactEmail) VALUES
+('Red Bull', 'John Doe', 'johndoe@gmail.com'),
+('Honda', 'Jane Doe', 'janedoe@gmail.com'),
+('Coca-Cola', 'Jack Doe', 'jackdoe@gmail.com'),
+('Chipotle', 'Mary Doe', 'marydoe@gmail.com');
+
+--
+-- Insert into 'Teams'
+--
+INSERT INTO Teams (TeamName, Captain, Location, SponsorID, TournamentID, Description) VALUES
+('TSM', 'John Doe', 'USA ', NULL, 3, 'Hello from TSM'),
+('C9', 'Jane Doe', 'Germany', 4, NULL, 'Hello from C9'),
+('Faze', 'Jack Doe', 'Mexico', 2, 2, 'Hello from Faze'),
+('T1', 'Mary Doe', 'South Korea', 1, 3, 'Hello from T1');
+
+--
+-- Insert into 'Players'
+--
+INSERT INTO Players (Name, Username, Birthdate, TeamID, GameID) VALUES
+('John Doe', 'team1user1', '2000-01-01', NULL, 3),
+('Jane Doe', 'team1user2', '1999-02-02', 1, NULL),
+('Jack Doe', 'team2user1', '1997-03-07', 2, 4),
+('Mary Doe', 'team4user1', '2000-03-09', 4, 2);
+
+SET FOREIGN_KEY_CHECKS = 1;
+COMMIT;
---
+++ DB_SQL/DMQ.sql
@@ -0,0 +1,67 @@
+-- Group 73 Project Step 3
+-- Steven Vu and Kevin Le
+
+--
+-- Database: `cs340_vust`
+-- Database: 'cs340_lekevinp
+--
+
+
+----- SELECTS -----
+-- get all Tournaments information
+SELECT Tournaments.TournamentID, Tournaments.TournamentName, Tournaments.Location, Tournaments.StartDate, Tournaments.EndDate from Tournaments;
+
+-- get all Players information
+SELECT Players.PlayerID, Players.Name, Players.Username, Players.Birthdate, IFNULL(Teams.TeamName, "*No Team*") as Team, IFNULL(Games.Title, "No Game") as Game FROM Players
+LEFT OUTER JOIN Teams ON Teams.TeamID=Players.TeamID
+LEFT OUTER JOIN Games ON Games.GameID=Players.GameID;
+
+-- get all Teams information
+SELECT Teams.TeamID, Teams.TeamName, Teams.Captain, Teams.Location, IFNULL(Sponsors.SponsorName, "*No Sponsor*") as Sponsor, IFNULL(Tournaments.TournamentName, "No Tournament") as Tournament, Teams.Description FROM Teams
+LEFT OUTER JOIN Sponsors ON Sponsors.SponsorID=Teams.SponsorID
+LEFT OUTER JOIN Tournaments ON Tournaments.TournamentID=Teams.TournamentID;
+
+-- get all Games information
+SELECT Games.GameID, Games.Title, Games.Genre, Games.Platform from Games;
+
+-- get all Sponsors information
+SELECT Sponsors.SponsorID, Sponsors.SponsorName, Sponsors.ContactPerson, Sponsors.ContactEmail from Sponsors;
+
+
+----- INSERTS -----
+-- add a new Tournament
+INSERT INTO Tournaments (TournamentName, Location, StartDate, EndDate)
+VALUES (:TournamentNameInput, :LocationInput, :StartDateInput, :EndDateInput);
+
+-- add a new Player
+INSERT INTO Players (Name, Username, Birthdate, TeamID, GameID)
+VALUES (:NameInput, :UsernameInput, :BirthdateInput, :TeamID_from_dropdown_Input, :GameID_from_dropdown_Input);
+
+-- add a new Team
+INSERT INTO Teams (TeamName, Captain, Location, SponsorID, TournamentID, Description)
+VALUES (:TeamNameInput, :Captain_from_dropdown_Input, :LocationInput, :SponsorID_from_dropdown_Input, :TournamentID_from_dropdown_Input, :DescriptionInput);
+
+-- add a new Game
+INSERT INTO Games (Title, Genre, Platform)
+VALUES (:TitleInput, :GenreInput, :PlatformInput);
+
+-- add a new Sponsor
+INSERT INTO Sponsors (SponsorName, ContactPerson, ContactEmail)
+VALUES (:SponsorNameInput, :ContactPersonInput, :ContactEmailInput);
+
+-- add Game to Tournament (M-M Relationship)
+INSERT INTO Tournaments_has_Games (GameID, TournamentID)
+VALUES (:GameID_from_dropdown_Input, :TournamentID_from_dropdown_Input);
+
+
+----- Update -----
+-- update Player information
+UPDATE Players SET Name = :NameInput, Username = :UsernameInput, Birthdate = :BirthdateInput, TeamID = :TeamID_from_dropdown_Input, GameID = :GameID_from_dropdown_Input WHERE PlayerID = :PlayerID_from_dropdown_Input;
+
+
+----- Delete -----
+-- delete a Player
+DELETE FROM Players WHERE PlayerID = :PlayerID_selected_from_browse_player_page;
+
+-- disassociate a Game from a Tournament
+DELETE FROM Tournaments_has_Games WHERE GameID = :GameID_selected_from_game_and_tournament_list AND TournamentID = :TournamentID_selected_from_game_and_tournament_list;
