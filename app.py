abd6b4f3ce1def73ca68bc31be2266a7ad99437b
Steven Vu <steven.vu0451@gmail.com>
2023-12-07 20:33:28 -0500

    Button changes

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ app = Flask(__name__)
 # app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 # database connection info
-app.config["MYSQL_HOST"] = ""
-app.config["MYSQL_USER"] = ""
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = ""
+app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
+app.config["MYSQL_USER"] = "cs340_vust"
+app.config["MYSQL_PASSWORD"] = "x62ZkepfILY7"
+app.config["MYSQL_DB"] = "cs340_vust"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
